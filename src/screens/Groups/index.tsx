8f1fe0503759719b45b7b5b197bec90f7da38e38
Thalys001 <thalyssouza708@gmail.com>
2023-10-25 23:15:28 -0300

    feat: creating list empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,11 +3,11 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-
 import { Container } from './styles'
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera do React']);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -24,6 +24,11 @@ export function Groups() {
           <GroupCard title={item}
           />
         )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message='Que tal cadastrar a primeira turma' />
+        )
+        }
       />
 
     </Container>
