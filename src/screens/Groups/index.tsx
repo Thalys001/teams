49d4d1a284a6afc875c46e9351c55c64158dcde1
Thalys001 <thalyssouza708@gmail.com>
2023-10-26 00:14:42 -0300

    feat: creating button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -5,6 +5,7 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles'
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -30,7 +31,9 @@ export function Groups() {
         )
         }
       />
-
+      <Button
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
