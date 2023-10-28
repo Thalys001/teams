5087e777795ab140ca1ebb5983f1306b957b0881
Thalys001 <thalyssouza708@gmail.com>
2023-10-27 22:45:53 -0300

    feat: creating input component

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 
 export function NewGroup() {
   return (
@@ -12,6 +13,9 @@ export function NewGroup() {
         <Highlight
           title="Nova Turma"
           subtitle="crie uma turma para adicionar pessoas"
+        />
+        <Input
+        placeholder="Nome da turma"
         />
         <Button
           title='Criar'
